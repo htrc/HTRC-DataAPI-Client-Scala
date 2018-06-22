31f4c5b70175cf7870f09fba05ed6a51cb3a04a9
Boris Capitanu <borice@hotmail.com>
2018-06-22 18:34:04 -0500

    Added ability to retrieve error info from ZIP without exception

--- src/main/scala/org/hathitrust/htrc/tools/dataapi/VolumeIterator.scala
+++ src/main/scala/org/hathitrust/htrc/tools/dataapi/VolumeIterator.scala
@@ -8,25 +8,26 @@ import org.hathitrust.htrc.tools.dataapi.exceptions.ApiRequestException
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.io.{Codec, Source}
-import scala.util.Try
 
 object VolumeIterator {
   private val logger: Logger = LoggerFactory.getLogger(getClass)
   private val pageSeqRegex = """[^/]+/\D*(\d+)\.txt""".r
 
   @SuppressWarnings(Array("org.wartremover.warts.Throw"))
-  def apply(f: File)(implicit codec: Codec): VolumeIterator = {
-    val zipFile = new ZipFile(f, codec.charSet)
-    Option(zipFile.getEntry("ERROR.err")) match {
-      case Some(entry) =>
-        val error = Source.fromInputStream(zipFile.getInputStream(entry)).mkString
-        Try(zipFile.close())
-        throw ApiRequestException(500, error)
-
-      case None =>
+  def apply(f: File, failOnError: Boolean = true)(implicit codec: Codec): VolumeIterator = {
+    if (failOnError) {
+      val zipFile = new ZipFile(f, codec.charSet)
+      try {
+        Option(zipFile.getEntry("ERROR.err")).foreach { entry =>
+          val error = Source.fromInputStream(zipFile.getInputStream(entry)).mkString
+          throw ApiRequestException(500, error)
+        }
+      } finally {
         zipFile.close()
-        new VolumeIterator(new ZipInputStream(new FileInputStream(f), codec.charSet))
+      }
     }
+
+    new VolumeIterator(new ZipInputStream(new FileInputStream(f), codec.charSet))
   }
 
   def apply(zipStream: ZipInputStream)(implicit codec: Codec): VolumeIterator =
@@ -34,7 +35,7 @@ object VolumeIterator {
 }
 
 class VolumeIterator(zipStream: ZipInputStream)
-                    (implicit codec: Codec) extends Iterator[HtrcVolume] with AutoCloseable {
+                    (implicit codec: Codec) extends Iterator[Either[HtrcVolume, DataApiError]] with AutoCloseable {
   import VolumeIterator._
 
   @SuppressWarnings(Array("org.wartremover.warts.Var"))
@@ -44,11 +45,7 @@ class VolumeIterator(zipStream: ZipInputStream)
 
   @SuppressWarnings(Array("org.wartremover.warts.Throw"))
   override def hasNext: Boolean = nextEntry match {
-    case Some(entry) if entry.isDirectory => true
-
-    case Some(entry) if entry.getName.equalsIgnoreCase("ERROR.err") =>
-      val error = Source.fromInputStream(zipStream).mkString
-      throw ApiRequestException(500, error)
+    case Some(entry) if entry.isDirectory || entry.getName.equalsIgnoreCase("ERROR.err") => true
 
     case Some(entry) =>
       logger.debug("Ignoring ZIP entry: {}", entry.getName)
@@ -63,7 +60,7 @@ class VolumeIterator(zipStream: ZipInputStream)
     "org.wartremover.warts.TryPartial",
     "org.wartremover.warts.Throw"
   ))
-  override def next(): HtrcVolume = nextEntry match {
+  override def next(): Either[HtrcVolume, DataApiError] = nextEntry match {
     case Some(zipEntry) if zipEntry.isDirectory =>
       val cleanId = zipEntry.getName.init
       val pages = Iterator
@@ -81,7 +78,11 @@ class VolumeIterator(zipStream: ZipInputStream)
         .sortBy(_.seq)
 
       val volumeId = HtrcVolumeId.parseClean(cleanId).get
-      new HtrcVolume(volumeId, pages)
+      Left(new HtrcVolume(volumeId, pages))
+
+    case Some(zipEntry) if zipEntry.getName.equalsIgnoreCase("ERROR.err") =>
+      val error = Source.fromInputStream(zipStream).mkString
+      Right(DataApiError(error))
 
     case _ => throw new NoSuchElementException
   }
